d204eb65e2d19a7c3e57d76e69de0233e71e6180
Chris Edwards <kryz2501@gmail.com>
2017-12-27 17:04:15 +0000

    Create gen_group_config.php

---
+++ gen_group_config.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+/*
+* LibreNMS auto generate oxidized group configuration from device group parameters
+* this should be outputted to /includes/config_ox_groups.php
+* and "require $install_dir . '/includes/config_ox_groups.php';"
+* added to the the main config.php to pull this configuration in
+*
+*/
+
+$init_modules = array();
+require realpath(__DIR__ . '/..') . '/includes/init.php';
+
+?>
+
+
+<?php
+$pgroup     = $config['distributed_poller_group'];
+echo "<?php" . PHP_EOL;
+echo PHP_EOL;
+foreach (dbFetchRows("SELECT `pattern`, REPLACE(name,' ','_') FROM `device_groups` WHERE name NOT LIKE '_Internal_Systems'") as $groups) {
+        $groupmatch = str_replace("%devices.sysName ~ \"", "'/^", $groups['pattern']);
+        $groupmatch = str_replace("%\"  &&", "/'", $groupmatch);
+        echo "\$config['oxidized']['group']['hostname'][] = array('regex' =>" . $groupmatch . ", 'group' => '" . $groups['REPLACE(name,\' \',\'_\')'] . "');" . PHP_EOL;
+}
