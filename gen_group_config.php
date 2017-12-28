d7f06dd1637c29866a5e1501c71958f5114448f3
Chris Edwards <kryz2501@gmail.com>
2017-12-28 09:41:34 +0000

    Update gen_group_config.php

--- gen_group_config.php
+++ gen_group_config.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 /*
-* LibreNMS auto generate oxidized group configuration from device group parameters
+* LibreNMS auto generate oxidized group configuration
 * this should be outputted to /includes/config_ox_groups.php
 * and "require $install_dir . '/includes/config_ox_groups.php';"
 * added to the the main config.php to pull this configuration in
@@ -10,16 +10,20 @@
 
 $init_modules = array();
 require realpath(__DIR__ . '/..') . '/includes/init.php';
-
 ?>
-
-
 <?php
 $pgroup     = $config['distributed_poller_group'];
 echo "<?php" . PHP_EOL;
-echo PHP_EOL;
 foreach (dbFetchRows("SELECT `pattern`, REPLACE(name,' ','_') FROM `device_groups` WHERE name NOT LIKE '_Internal_Systems'") as $groups) {
-        $groupmatch = str_replace("%devices.sysName ~ \"", "'/^", $groups['pattern']);
-        $groupmatch = str_replace("%\"  &&", "/'", $groupmatch);
-        echo "\$config['oxidized']['group']['hostname'][] = array('regex' =>" . $groupmatch . ", 'group' => '" . $groups['REPLACE(name,\' \',\'_\')'] . "');" . PHP_EOL;
+	$groupmatch = str_replace("%devices.sysName ~ \"", "'/^", $groups['pattern']);
+	$groupmatch = str_replace("%\"", "/'", $groupmatch);
+        $groupmatch = str_replace("&&", "", $groupmatch);
+        $groupmatch = str_replace("  ", "", $groupmatch);
+	if (empty($groupmatch)) {
+		/* donothing */
+		}
+	else {
+		echo "\$config['oxidized']['group']['hostname'][] = array('regex' =>" . $groupmatch . ", 'group' => '" . $groups['REPLACE(name,\' \',\'_\')'] . "');" . PHP_EOL;
+	}
 }
+echo "?>";
