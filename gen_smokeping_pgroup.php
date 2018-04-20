baca76fceab406facacbbe0b7ac8d880b8aa87de
Chris Edwards <kryz2501@gmail.com>
2018-04-20 17:30:01 +0100

    Update gen_smokeping_pgroup.php

--- gen_smokeping_pgroup.php
+++ gen_smokeping_pgroup.php
@@ -21,18 +21,22 @@ require realpath(__DIR__ . '/..') . '/includes/init.php';
 
 <?php
 $pgroup     = $config['distributed_poller_group'];
+$probe_id = 1;
 foreach (dbFetchRows("SELECT `id`, REPLACE(name,' ','_') FROM `device_groups` WHERE name NOT LIKE '_Internal_Systems'") as $groups) {
     	echo '+ ' . $groups['REPLACE(name,\' \',\'_\')'] . PHP_EOL;
     	echo 'menu = ' . $groups['REPLACE(name,\' \',\'_\')'] . PHP_EOL;
     	echo 'title = ' . $groups['REPLACE(name,\' \',\'_\')'] . PHP_EOL;
 	echo PHP_EOL;
-	foreach (dbFetchRows("select hostname, type from `devices` inner join `device_group_device` on devices.device_id = device_group_device.device_id where device_group_device.device_group_id = ? AND `ignore` = 0 AND `poller_group` = '$pgroup'  AND`disabled` = 0", array($groups['id'])) as $devices) {
+	foreach (dbFetchRows("select hostname , type from `devices` inner join `device_group_device` on devices.device_id = device_group_device.device_id where device_group_device.device_group_id = ? AND `ignore` = 0 AND `poller_group` = '$pgroup'  AND`disabled` = 0", array($groups['id'])) as $devices) {
         	//Dot needs to be replaced, since smokeping doesn't accept it at this level
         	echo '++ ' . str_replace(".", "_", $devices['hostname']) . PHP_EOL;
         	echo 'menu = ' . $devices['hostname'] . PHP_EOL;
         	echo 'title = ' . $devices['hostname'] . PHP_EOL;
-        	echo 'host = ' . $devices['hostname'] . PHP_EOL . PHP_EOL;
-		echo 'ln -s' . $config['smokeping']['dir'] . $groups['REPLACE(name,\' \',\'_\')'] . '/' . $devices['hostname'] . $config['smokeping']['dir'] . $devices['type'] . '/' . $devices['hostname'];
-
+                echo 'probe = FPing' . $probe_id . PHP_EOL;
+        	echo 'host = ' . $devices['hostname'] .PHP_EOL .  PHP_EOL;
+		$probe_id++;
+		if ($probe_id > 4 ) {
+			$probe_id = 1;
+			}
     	}
 }
